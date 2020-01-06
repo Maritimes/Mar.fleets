4878d3635e32b8d7042e201703ace63e79a6df0a
McMahon <McMahonM@dfo-mpo.ca>
2020-01-06 19:34:31 +0000

    nothing - just clearing commits

--- R/calc_Coverage.R
+++ R/calc_Coverage.R
@@ -112,6 +112,7 @@ calc_Coverage<-function(get_OBS = NULL,
 
   #by_trip
   if (!quietly)cat(paste0("\n", "Figuring out the area in which the most sets occurred during each trip.","\n"))
+
   if (!is.null(get_OBS$OBS_TRIPS) && !is.null(get_OBS$OBS_SETS)){
     O_trips = merge(get_OBS$OBS_TRIPS[,!names(get_OBS$OBS_TRIPS) %in% c("BOARD_DATE","LANDING_DATE")],
                     get_OBS$OBS_SETS, all.y =TRUE, by.x = "TRIP_ID_OBS", by.y = "TRIP_ID")
