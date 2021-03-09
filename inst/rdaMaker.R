0ca70212a61b54c1ee306e56364c7fbea3926f50
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-03-09 16:11:22 +0000

    updating associated documentation

--- inst/rdaMaker.R
+++ inst/rdaMaker.R
@@ -1,7 +1,7 @@
 licCore =  data.frame(readxl::read_xlsx( path = "fleetDefnsCore.xlsx",sheet = "Sheet1"))
-licSizes =  data.frame(readxl::read_xlsx( path = "fleetDefnsSizes.xlsx",sheet = "Sheet1"))
+licGearSizes =  data.frame(readxl::read_xlsx( path = "fleetDefnsSizes.xlsx",sheet = "Sheet1"))
 licAreas =  data.frame(readxl::read_xlsx( path = "fleetDefnsAreas.xlsx",sheet = "Sheet1"))
 
-save(licCore, file = "data/licCore.rda")
-save(licSizes, file = "data/licSizes.rda")
-save(licAreas, file = "data/licAreas.rda")
+save(licCore, file = "../data/licCore.rda")
+save(licGearSizes, file = "../data/licGearSizes.rda")
+save(licAreas, file = "../data/licAreas.rda")
