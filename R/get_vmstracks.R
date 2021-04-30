990e13f8e18010af36dad9d44f6e060e89fdb58f
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-04-30 17:49:07 +0100

    simplifies get_vmstracks params

--- R/get_vmstracks.R
+++ R/get_vmstracks.R
@@ -1,12 +1,7 @@
 #' @ title get_vmstracks
 #' @description This function takes the results from get_marfis() and get_isdb()
 #' and extracts the relevant VMS tracks, and flags whether or not each was observed
-#' @param get_marfis default is \code{NULL}. This is the list output by the
-#' \code{Mar.bycatch::get_marfis()} function - it contains dataframes of both the
-#' trip and set information from MARFIS
-#' @param get_isdb default is \code{NULL}. This is the list output by the
-#' \code{Mar.bycatch::get_isdb()} function - it contains dataframes of both the
-#' trip and set information from the ISDB database.
+#' @param data default is \code{NULL}. This is the entire output from any of the fleet wrappers.
 #' @param ... other arguments passed to methods
 #' It is likely that you will need to provide values for the following 4
 #' parameters to enable a connection to oracle:
@@ -22,7 +17,8 @@
 #' @return returns a dataframe of the VMS data.  The OBS field contains a value>0 if the trip was observed.
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-get_vmstracks<-function(get_marfis = NULL, get_isdb = NULL, ...){
+get_vmstracks<-function(data = NULL, ...){
+
   argsUser <- list(...)
   args <- do.call(set_defaults, list(argsUser=argsUser))$args
   if (args$useLocal==TRUE){
@@ -37,16 +33,16 @@ get_vmstracks<-function(get_marfis = NULL, get_isdb = NULL, ...){
                                                                 stringsAsFactors=FALSE)
 
 
-  marDat<-merge(get_marfis$MARF_SETS, get_marfis$MARF_TRIPS, by.x = c("MON_DOC_ID","TRIP_ID_MARF") , by.y=c("MON_DOC_ID","TRIP_ID_MARF"), all.x=T)
+  marDat<-merge(data$marf$MARF_SETS, data$marf$MARF_TRIPS, by.x = c("MON_DOC_ID","TRIP_ID_MARF") , by.y=c("MON_DOC_ID","TRIP_ID_MARF"), all.x=T)
 
   vr_dates1 <- cbind(marDat[,c("VR_NUMBER_FISHING", "EF_FISHED_DATETIME")],0)
   vr_dates2 <- cbind(marDat[,c("VR_NUMBER_LANDING", "EF_FISHED_DATETIME")],0)
   colnames(vr_dates1)<-colnames(vr_dates2)<-c("VR_NUMBER","mDate", "OBS")
 
-  if(!is.null(get_isdb)){
-    if (nrow(get_isdb$ISDB_TRIPS[!is.na(get_isdb$ISDB_TRIPS$TRIP_ID_MARF),])>0 && nrow(get_isdb$ISDB_TRIPS[!is.na(get_isdb$ISDB_SETS$TRIP_ID_MARF),])>0){
-      obsDat<-merge(get_isdb$ISDB_TRIPS[!is.na(get_isdb$ISDB_TRIPS$TRIP_ID_MARF),c("TRIP_ID_ISDB", "VR")],
-                    get_isdb$ISDB_SETS[,c("TRIP_ID","DATE_TIME")],
+  if(nrow(data$isdb$ISDB_TRIPS)>0 & nrow(data$isdb$ISDB_SETS)>0 ){
+    if (nrow(data$isdb$ISDB_TRIPS[!is.na(data$isdb$ISDB_TRIPS$TRIP_ID_MARF),])>0 && nrow(data$isdb$ISDB_TRIPS[!is.na(data$isdb$ISDB_SETS$TRIP_ID_MARF),])>0){
+      obsDat<-merge(data$isdb$ISDB_TRIPS[!is.na(data$isdb$ISDB_TRIPS$TRIP_ID_MARF),c("TRIP_ID_ISDB", "VR")],
+                    data$isdb$ISDB_SETS[,c("TRIP_ID","DATE_TIME")],
                     by.x= "TRIP_ID_ISDB", by.y ="TRIP_ID", all.Y=T)
 
       vr_dates3 <- cbind(obsDat[,c("VR","DATE_TIME")],1)
@@ -57,8 +53,8 @@ get_vmstracks<-function(get_marfis = NULL, get_isdb = NULL, ...){
   vr_dates<-rbind(vr_dates1, vr_dates2)
   vr_dates<-rbind(vr_dates, vr_dates3)
 
+  vr_dates <- vr_dates[!is.na(vr_dates$mDate) & !is.na(vr_dates$VR_NUMBER),]
 
-  vr_dates <- vr_dates[!is.na(vr_dates$mDate),]
   vr_dates<-unique(vr_dates)
   theDates<- as.Date(range(vr_dates$mDate))
   allVRs <- unique(vr_dates$VR_NUMBER)
