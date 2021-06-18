85997d263e8afe99453bfd6e56842c375aff0058
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-06-18 15:05:55 +0100

    adds NAFO as an aggregator (ie parameter)

--- R/marfisSummarizer.R
+++ R/marfisSummarizer.R
@@ -12,6 +12,9 @@
 #' @param byYr  default is \code{TRUE}.   If TRUE, the summed weights and numbers of unique trips
 #' and licence_ids will will be broken down by each year in which a landing was identified. If FALSE,
 #' weights will be summed, irrespective of year.
+#' @param byNAFO  default is \code{FALSE}.   If TRUE, the summed weights and numbers of unique trips
+#' and licence_ids will will be broken down by <reported> NAFO division in which a landing was
+#' identified. If FALSE, weights will be summed, irrespective of <reported> NAFO division.
 #' @examples \dontrun{
 #' summary <- marfisSummarizer(data = Halibut2017, byGr = TRUE)
 #'        }
@@ -19,10 +22,14 @@
 #' @return a data frame
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-marfisSummarizer <- function(data=NULL, byGr = TRUE, bySpp = TRUE, byYr= TRUE){
+marfisSummarizer <- function(data=NULL, tonnes=F, byGr = TRUE, bySpp = TRUE, byYr= TRUE, byNAFO = FALSE){
   t <- data$marf$MARF_TRIPS
   c <- data$marf$MARF_CATCHES
+  n <- unique(data$marf$MARF_SETS[,c("LOG_EFRT_STD_INFO_ID", "NAFO_MARF_SETS")])
+  colnames(n)[colnames(n)=="NAFO_MARF_SETS"] <- "NAFO"
+
   all <- merge(t, c)
+  all <- merge(all, n)
   all$YEAR <- lubridate::year(all$T_DATE1)
   thisVessN <- length(unique(all$VR_NUMBER_FISHING))
   thisLicsN <- length(unique(all$LICENCE_ID))
@@ -30,8 +37,8 @@ marfisSummarizer <- function(data=NULL, byGr = TRUE, bySpp = TRUE, byYr= TRUE){
 
   res <- data.frame("NTRIPS" = thisTripsN, "NVESS"= thisVessN, "NLICS"= thisLicsN)
 
-  if (byGr | bySpp | byYr){
-    potFields <- c("GEAR_CODE", "SPECIES_CODE", "YEAR", "LICENCE_ID", "TRIP_ID_MARF","VR_NUMBER_FISHING")
+  if (byGr | bySpp | byYr | byNAFO){
+    potFields <- c("GEAR_CODE", "SPECIES_CODE", "YEAR", "LICENCE_ID", "TRIP_ID_MARF","VR_NUMBER_FISHING", "NAFO")
     aggFields <- c("RND_WEIGHT_KGS")
     aggFields <- c(aggFields, "LICENCE_ID")
     aggFields <- c(aggFields, "VR_NUMBER_FISHING")
@@ -39,6 +46,7 @@ marfisSummarizer <- function(data=NULL, byGr = TRUE, bySpp = TRUE, byYr= TRUE){
     if(byGr) aggFields <- c(aggFields, "GEAR_CODE")
     if(bySpp) aggFields <- c(aggFields, "SPECIES_CODE")
     if(byYr) aggFields <- c(aggFields, "YEAR")
+    if(byNAFO) aggFields <- c(aggFields, "NAFO")
     potFields <- potFields[!potFields %in% aggFields]
     all <- all[,colnames(all) %in% aggFields]
     thisAgg = aggregate(RND_WEIGHT_KGS ~ ., data = all, FUN = sum)
@@ -62,11 +70,14 @@ marfisSummarizer <- function(data=NULL, byGr = TRUE, bySpp = TRUE, byYr= TRUE){
     colnames(res)[colnames(res)=="TRIP_ID_MARF"] <- "NTRIPS"
     res[,potFields]<-"ALL"
   }else{
-    res <- data.frame("YEAR" = "ALL", "NTRIPS" = thisTripsN, "NVESS"= thisVessN, "NLICS"= thisLicsN, "GEAR_CODE"="ALL", "SPECIES_CODE" = "ALL", "RND_WEIGHT_KGS"= sum(all$RND_WEIGHT_KGS))
+    res <- data.frame("YEAR" = "ALL", "NTRIPS" = thisTripsN, "NVESS"= thisVessN, "NLICS"= thisLicsN, "GEAR_CODE"="ALL", "SPECIES_CODE" = "ALL", "NAFO" = "ALL", "RND_WEIGHT_KGS"= sum(all$RND_WEIGHT_KGS))
   }
-  allFields <- c("YEAR","NTRIPS", "NVESS", "NLICS", "GEAR_CODE", "SPECIES_CODE","RND_WEIGHT_KGS")
+  allFields <- c("YEAR","NTRIPS", "NVESS", "NLICS", "GEAR_CODE", "SPECIES_CODE","NAFO","RND_WEIGHT_KGS")
 
   res<- res[,allFields]
   res<- res[with(res,order(YEAR, GEAR_CODE, NTRIPS)),]
+  if (tonnes){
+    res$RND_WEIGHT_TONNES <- res$RND_WEIGHT_KGS/1000
+  }
   return(res)
 }
