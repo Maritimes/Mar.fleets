cb1e50a7b4d784d00a75186ded5e31d5cadea127
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-07-21 20:08:39 +0100

    communication w groundfish group resulted in breaking areas for fishin_CHPs() in 5Z and 4X5Y - modified wrapper, and licence tables

--- R/fishin_CHPs.R
+++ R/fishin_CHPs.R
@@ -8,12 +8,15 @@
 #' @param marfGear default is \code{''}. This is a vector of MARFIS gear codes known to have caught
 #' this species. The default values can be replaced with a smaller selection to only return information
 #' for a gear-specific subset of fishing activity.
-#' @param type default is \code{NULL}. This is either "FIXED" or "MOBILE".
+#' @param type default is \code{NULL}. This is either "ALL", "FIXED" or "MOBILE". "ALL" just gets
+#' results for all gear types
+#' @param stock default is \code{NULL}. This is either "4X5Y" or "5Z".
 #' @inherit set_defaults params
 #' @inheritDotParams set_defaults -lics -gearSpecs -area
 #' @examples \dontrun{
 #' db <- fishin_CHPs(type = "FIXED",
-#'                     year = 2018,
+#'                     dateStart =  "2016-04-01", dateEnd= "2017-03-31",
+#'                     stock = "5Z",
 #'                     useLocal = F,
 #'                     oracle.username = "<name>",
 #'                     oracle.password="<password>",
@@ -21,6 +24,8 @@
 #'                     usepkg = "roracle"
 #'                     )
 #' local <- fishin_CHPs(type = "MOBILE",
+#'                        dateStart =  "2016-04-01", dateEnd= "2017-03-31",
+#'                        stock = "4X5Y",
 #'                        year = 2018,
 #'                        useLocal = T,
 #'                        data.dir = "c:/data_folder"
@@ -33,23 +38,26 @@
 #' \itemize{
 #'   \item \code{marfSpp} = c(100, 110, 170)
 #'   \item \code{isdbSpp} = c(10, 11, 16)
-#'   \item \code{tripcd_id} =
-#'   \item \code{fleet} =
-#'   \item \code{area} =
-#'   \item \code{gearSpecs} =
+#'   \item \code{tripcd_id} = c(10, 7001)
+#'   \item \code{fleet} = "CHP"
+#'   \item \code{stock} = 5Z or 4X5Y
 #' }
 #' @inherit fleet_ details
 #' @export
-fishin_CHPs <- function(marfGear = c(12, 21, 41, 51, 59, 62), type=NULL, useLocal = NULL, ...){
+fishin_CHPs <- function(marfGear = c(12, 21, 41, 51, 59, 62), type=NULL, stock = NULL, useLocal = NULL, ...){
   if(!paramOK(useLocal = useLocal, p=list(...))) stop("Please provide additional parameters as directed above")
   type <- toupper(type)
+  area <- toupper(stock)
 
   if (!is.null(type) && type=="MOBILE"){
     marfGear = c(12)
   }else if (!is.null(type) && type=="FIXED"){
     marfGear = c(41,51,59)
+  }else if (!is.null(type) && type=="ALL"){
+    #just get all gear
   }
 
-  data <- fleet_(fleet = "CHP", marfSpp = c(100, 110, 170), isdbSpp = c(10, 11, 16), marfGear = marfGear, tripcd_id = c(10, 7001), area= "4X5YZ", areaFileField = "COD", useLocal = useLocal,...)
+
+  data <- fleet_(fleet = "CHP", marfSpp = c(100, 110, 170), isdbSpp = c(10, 11, 16), marfGear = marfGear, tripcd_id = c(10, 7001), area= area, areaFileField = "COD", useLocal = useLocal,...)
   return(data)
 }
