c8e9efd2e4e2de2be390886a0b93f50cb0d75188
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2021-07-21 20:09:18 +0100

    better handles old results

--- R/summarizer.R
+++ R/summarizer.R
@@ -1,25 +1,32 @@
 #' @title summarizer
-#' @description This is a simple tool that quickly breaks down the results of any of the fleet
+#' @description This is a tool that quickly breaks down the results of any of the fleet
 #' wrappers to provide information on the number of trips, unique vessels, unique licences, and total
-#' catch.  These results can be aggregated by YEAR, SPECIES_CODE, and GEAR TYPE.
+#' catch.  These results can be further aggregated by Year, NAFO area, Species code, GEAR TYPE, as
+#' well as by any fields that exist in the TRIPS or SETS object of the marfis or isdb results
+#' (or both).
 #' @param data  default is \code{NULL}. This is the entire output from any of the fleet wrappers.
 #' @param units  default is \code{"KGS"}. This is the units you want weights returned in.  Valid
 #' options are any of "KGS", "TONNES", "LBS".
 #' @param byGr  default is \code{TRUE}. If TRUE, the summed weights and numbers of unique trips and
-#' licence_ids will will be broken down by all identified gear types. If FALSE,
-#' weights will be summed, irrespective of gear.
+#' licence_ids will will be broken down by all identified gear types. If FALSE, aggregations will
+#' ignore differences in gear types.
 #' @param bySpp  default is \code{TRUE}. If TRUE, the summed weights and numbers of unique trips and
-#' licence_ids will will be broken down by all identified landed species. If FALSE,
-#' weights will be summed, irrespective of landed species.
-#' @param byYr  default is \code{TRUE}.   If TRUE, the summed weights and numbers of unique trips
+#' licence_ids will will be broken down by all identified landed species. If FALSE,aggregations will
+#' ignore differences in landed species.
+#' @param byYr  default is \code{FALSE}.   If TRUE, the summed weights and numbers of unique trips
 #' and licence_ids will will be broken down by each year in which a landing was identified. If FALSE,
-#' weights will be summed, irrespective of year. "Year" is taken from the end date of the associated
-#' trip record.
+#' aggregations will ignore differences in year. "Year" is taken from the end date of the associated
+#' trip record (i.e. <data>$marf$MARF_SETS$T_DATE2 for MARF, and <data>$isdb$ISDB_SETS$DATE_TIME for
+#' ISDB).
 #' @param byNAFO  default is \code{FALSE}.   If TRUE, the summed weights and numbers of unique trips
 #' and licence_ids will will be broken down by <reported> NAFO division in which a landing was
 #' identified. If FALSE, weights will be summed, irrespective of <reported> NAFO division.
 #' @param byCust  default is \code{NULL}. This can be the name of any field found in \code{<data>$marf$MARF_SETS}
 #' by which you would like to see aggregated results
+#' @param specISDB default is \code{NULL}.  If your data is for a fleet returns multiple species, you can
+#' enter an ISDB species code here, and the ISDB data will be limited to those species.
+#' @param specMARF default is \code{NULL}.  If your data is for a fleet returns multiple species, you can
+#' enter an MARFIS species code here, and the MARF data  will be limited to those species.
 #' @param doMARF  default is \code{TRUE}. This indicates that the MARFIS results should be summarized.
 #' @param doISDB  default is \code{TRUE}. This indicates that the ISDB results should be summarized.
 #' @param quietly default is \code{FALSE}. This specifies whether or not status messages should be
@@ -28,10 +35,12 @@
 #' summary <- summarizer(data = Halibut2017, byGr = TRUE)
 #'        }
 #' @family simpleproducts
-#' @return a data frame
+#' @return by default, a list containing an object with the results of both MARF and ISDB is
+#' returned.  If either \code{doMARF} or \code{doISDB} is set to FALSE, than the returned object is
+#' a dataframe
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 #' @export
-summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = TRUE, byNAFO = FALSE, byCust = NULL, speccd_id = NULL, doMARF=T, doISDB=T, quietly = F){
+summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= FALSE, byGr = TRUE, byNAFO = FALSE, byCust = NULL, specMARF = NULL, specISDB = NULL, doMARF=T, doISDB=T, quietly = F){
   defYr <- lubridate::year(as.Date(gsub('"',"",data$params$user[data$params$user$PARAMETER == "dateStart", "VALUE"]), format="%Y-%m-%d"))
   summISDB <- function(){
     byCustISDB <-NULL
@@ -39,30 +48,30 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
     s <- data$isdb$ISDB_SETS
     s$YEAR <- lubridate::year(s$DATE_TIME)
 
-    if (is.null(speccd_id)) {
-      speccd_id <- eval(parse(text=data$params$user[data$params$user$PARAMETER == "isdbSpp", "VALUE"]))
-      # if (!quietly) message(paste0("No speccd_id was specified - defaulting to wrapper target species"))
+    if (is.null(specISDB)) {
+      specISDB <- eval(parse(text=data$params$user[data$params$user$PARAMETER == "isdbSpp", "VALUE"]))
+      # if (!quietly) message(paste0("No specISDB was specified - defaulting to wrapper target species"))
     }
 
-    c <- data$isdb$ISDB_CATCHES$ALL[data$isdb$ISDB_CATCHES$ALL$SPECCD_ID %in% speccd_id,c("SPECCD_ID", "FISHSET_ID", "EST_NUM_CAUGHT", "EST_KEPT_WT", "EST_DISCARD_WT", "EST_COMBINED_WT")]
+    c <- data$isdb$ISDB_CATCHES$ALL[data$isdb$ISDB_CATCHES$ALL$SPECCD_ID %in% specISDB,c("SPECCD_ID", "FISHSET_ID", "EST_NUM_CAUGHT", "EST_KEPT_WT", "EST_DISCARD_WT", "EST_COMBINED_WT")]
 
 
     if (any(is.na(t$VR))){
       badVR <- length(t[is.na(t$VR),])
-      if (!quietly)  message(paste0(badVR, " of your trips were missing a valid Vessel. There is no way to
+      if (!quietly)  message(paste0(badVR, " of your ISDB trips were missing a valid Vessel. There is no way to
                      differentiate between missing vessels, so all missing values will be considered
                      as a single vessel, identified as '-999'"))
       t[["VR"]][is.na(t[["VR"]])] <- -999
     }
     if (any(is.na(t$LIC))){
       badLic <- length(t[is.na(t$LIC),])
-      if (!quietly) message(paste0(badLic, " of your trips were missing a valid LICENCE. There is no way to
+      if (!quietly) message(paste0(badLic, " of your ISDB trips were missing a valid LICENCE. There is no way to
                      differentiate between missing licences, so all missing values will be considered
                      as a single licence, identified as '-999'"))
       t[["LIC"]][is.na(t[["LIC"]])] <- -999
     }
 
-    all <- merge(t, s, by.x="TRIP_ID_ISDB", by.y="TRIP_ID")
+    all <- merge(t, s[, !names(s) %in% c("TRIP_ID_MARF")], by.x="TRIP_ID_ISDB", by.y="TRIP_ID")
     all <- merge(all, c[, !names(c) %in% c("TRIP_ID_MARF")], by="FISHSET_ID")
     rm(list=c("t","s","c"))
     sumFields <- c("EST_NUM_CAUGHT","EST_KEPT_WT","EST_DISCARD_WT","EST_COMBINED_WT")
@@ -82,7 +91,11 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
       all$SPECCD_ID  <- "ALL"
     }
     if(byGr) {
-      aggFields <- c(aggFields, "GEARCD_ID")
+      if ("GEARCD_ID" %in% names(all)){
+        aggFields <- c(aggFields, "GEARCD_ID")
+      }else{
+        message("Can't aggregate ISDB data by Gear code - the data appears to precede its availability")
+      }
     }else{
       all$GEARCD_ID <- "ALL"
     }
@@ -95,7 +108,7 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
         aggFields <- c(aggFields, byCustISDB)
         if (any(is.na(all[[byCustISDB]]))){
           badCust <- length(is.na(all[[byCustISDB]]))
-          if (!quietly) message(paste0(badCust, " of the records of your custom field were missing values. There is no way to
+          if (!quietly) message(paste0(badCust, " of the ISDB records of your custom field were missing values. There is no way to
                      differentiate between these, so allwill be considered as a single entity, identified as '-999'"))
           all[[byCustISDB]][is.na(all[[byCustISDB]])] <- -999
         }
@@ -103,7 +116,6 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
     }else{
       byCustISDB <- NULL
     }
-
     all <- all[,c(aggFields, sumFields, countFields)]
 
     sums <- all[, c(sumFields, aggFields)]
@@ -127,23 +139,42 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
 
   summMARF <- function(){
     byCustMARF <- NULL
-    # if(!"MARF_CATCHES" %in% names(data$marf)){
-    #   all <- data$marf$MARF_TRIPS
-    #   colnames(all)[colnames(all)=="NAFO_MARF_TRIPS"] <- "NAFO"
-    #   all$SPECIES_CODE <- "-9"
-    # }else{
     t <- data$marf$MARF_TRIPS
     t$YEAR <- lubridate::year(t$T_DATE2)
+    if (any(is.na(t$VR_NUMBER_FISHING))){
+      badVR <- length(t[is.na(t$VR_NUMBER_FISHING),])
+      if (!quietly)  message(paste0(badVR, " of your MARFIS trips were missing a valid Vessel. There is no way to
+                     differentiate between missing vessels, so all missing values will be considered
+                     as a single vessel, identified as '-999'"))
+      t[["VR"]][is.na(t[["VR"]])] <- -999
+    }
+    if (any(is.na(t$LICENCE_ID))){
+      badLic <- length(t[is.na(t$LICENCE_ID),])
+      if (!quietly) message(paste0(badLic, " of your MARFIS trips were missing a valid LICENCE. There is no way to
+                     differentiate between missing licences, so all missing values will be considered
+                     as a single licence, identified as '-999'"))
+      t[["LICENCE_ID"]][is.na(t[["LICENCE_ID"]])] <- -999
+    }
+
     s <- data$marf$MARF_SETS[, !(names(data$marf$MARF_SETS) %in% c("MON_DOC_ID" , "T_DATE1", "T_DATE2"))]
 
-    c <- data$marf$MARF_CATCHES
+    if (is.null(specMARF)) {
+      specMARF <- eval(parse(text=data$params$user[data$params$user$PARAMETER == "marfSpp", "VALUE"]))
+      # if (!quietly) message(paste0("No specMARF was specified - defaulting to wrapper target species"))
+    }
 
     all <- merge(t, s)
-    all <- merge(all, c)
-    rm(list=c("t","s","c"))
+    rm(list=c("t","s"))
+    if(!"MARF_CATCHES" %in% names(data$marf)){
+      if (bySpp) message("Can't aggregate MARFIS data by species - the data appears to precede its availability")
+      all$SPECIES_CODE <- "ALL"
+    }else{
+      c <- data$marf$MARF_CATCHES[data$marf$MARF_CATCHES$SPECIES_CODE %in% specMARF,]
+      all <- merge(all, c)
+      rm(list=c("c"))
+    }
 
     sumFields <- c("RND_WEIGHT_KGS")
-    # potFields <- c("YEAR","NAFO_MARF_SETS", "SPECIES_CODE", "GEAR_CODE")
     countFields <- c("TRIP_ID_MARF","VR_NUMBER_FISHING","LICENCE_ID")
     aggFields <-c("YEAR")
 
@@ -176,7 +207,7 @@ summarizer <- function(data=NULL, units="KGS", bySpp = TRUE, byYr= TRUE, byGr = 
         aggFields <- c(aggFields, byCustMARF)
         if (any(is.na(all[[byCustMARF]]))){
           badCust <- length(is.na(all[[byCustMARF]]))
-          if (!quietly) message(paste0(badCust, " of the records of your custom field were missing values. There is no way to
+          if (!quietly) message(paste0(badCust, " of the MARFIS records of your custom field were missing values. There is no way to
                      differentiate between these, so allwill be considered as a single entity, identified as '-999'"))
           all[[byCustMARF]][is.na(all[[byCustMARF]])] <- -999
         }
